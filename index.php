c76de9608694e6bf4c44a56f25e50bd2f9499158
Alvaro Carreno C <c2alvaro91@gmail.com>
2020-03-24 08:46:18 -0500

    commit 05 Primer comit despues de subir a GitHub

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
     echo "Hola Mundo \n";
-    echo "Linea nueva despues del commit 01";
+    echo "Linea nueva despues del commit 01 \n";
+    echo "Linea nueva despues de subir el repo a GitHub"
 ?>
